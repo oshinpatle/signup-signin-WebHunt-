21fa036364174f041af8e9ff71b7da87c7315a1d
roneetsingh <kaket786@gmail.com>
2022-01-22 16:38:43 +0530

    Merge Push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './App';
 
 ReactDOM.render(
@@ -8,14 +7,4 @@ ReactDOM.render(
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-=======
-import './index.css';
-import App from './App';
-
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById('root')
->>>>>>> 9959cb8a7d1607d4ea5f4b8f396be23ca52de1af
 );
